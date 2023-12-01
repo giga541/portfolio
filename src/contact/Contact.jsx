aedf2eb17c839604205ad6670320ed7fb11572a6
George <gigamikasha@gmail.com>
2023-12-01 22:24:04 +0400

    added sendEmail function

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import "./contact.scss";
 import { motion } from "framer-motion";
 import Svg from "./Svg";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const variants = {
   initial: { y: 500, opacity: 0 },
@@ -12,6 +14,49 @@ const variants = {
 };
 
 const Contact = () => {
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  const sendEmail = e => {
+    e.preventDefault();
+
+    if (enteredMessage.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        "service_xz4nztl",
+        "template_44txpiu",
+        formRef.current,
+        "RSsfeFL1smjajlFS4"
+      )
+      .then(
+        result => {
+          setSuccess(true);
+          setError(false);
+
+          setTimeout(() => {
+            setSuccess(false);
+          }, 3000);
+        },
+        error => {
+          setError(true);
+          setSuccess(false);
+        }
+      );
+
+    setEnteredName("");
+    setEnteredEmail("");
+    setEnteredMessage("");
+  };
+
   return (
     <motion.div
       className="contact"
@@ -32,11 +77,35 @@ const Contact = () => {
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 4, duration: 1 }}
+          ref={formRef}
+          onSubmit={sendEmail}
         >
-          <input type="text" required placeholder="Name" />
-          <input type="email" required placeholder="Mail" />
-          <textarea rows={8} placeholder="Message"></textarea>
+          <input
+            type="text"
+            required
+            placeholder="Name"
+            name="name"
+            value={enteredName}
+            onChange={e => setEnteredName(e.target.value)}
+          />
+          <input
+            type="email"
+            required
+            placeholder="Mail"
+            name="email"
+            value={enteredEmail}
+            onChange={e => setEnteredEmail(e.target.value)}
+          />
+          <textarea
+            rows={8}
+            placeholder="Message"
+            name="message"
+            value={enteredMessage}
+            onChange={e => setEnteredMessage(e.target.value)}
+          />
           <button>Submit</button>
+          {error && <p>Error: Please enter a message</p>}
+          {success && <p>Message has been successfully sent</p>}
         </motion.form>
       </div>
     </motion.div>
