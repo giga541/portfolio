c380f3655310a4d7a55f0bd855a58e8bfb0af313
George <gigamikasha@gmail.com>
2023-12-07 23:44:14 +0400

    hero section and services got mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,8 @@ function App() {
       <section id="Projects">
         <Projects />
       </section>
-      <section id="About">About</section>
-      <section id="Contact">
-        <Contact />
-      </section>
+      {/* <section id="About">About</section> */}
+      <section id="Contact">{/* <Contact /> */}</section>
     </div>
   );
 }
